71aee2417a5219db6e95e379b3a8dcf17c6ec49d
zoltankalmar <64471573+zoltankalmar@users.noreply.github.com>
2022-03-17 22:23:30 +0100

    Create bug.php

---
+++ bug.php
@@ -0,0 +1,10 @@
+class MyException extends Exception {}
+class MySubException extends MyException {}
+
+try {
+  doSomething();
+} catch (MyException $e) {
+  echo $e;
+} catch (MySubException $e) { // Noncompliant: MySubException is a subclass of MyException
+  echo "Never executed";
+}
